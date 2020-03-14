e7df90b7b8b52936eede4d36a026ae9c674eb69f
garricknorthover <garricknorthover@gmail.com>
2020-03-14 22:04:33 +1300

    added index.js

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-const moves = {
-    diagonalLeft: 'diagonalLeft',
-    diagonalRight: 'diagonalRight',
-    backDiagonalLeft: 'backDiagonalLeft',
-    backDiagonalRight: 'backDiagonalRight',
-    up: 'up',
-    back: 'back',
-    left: 'left',
-    right: 'right',
-}
-const { diagonalLeft, diagonalRight, backDiagonalLeft, backDiagonalRight, up, back, left, right } = moves
+// const moves = {
+//     diagonalLeft: (a, b) => [++a, --b],
+//     diagonalRight: (a, b) => [++a, ++b],
+//     backDiagonalLeft: 'backDiagonalLeft',
+//     backDiagonalRight: 'backDiagonalRight',
+//     up: (a, b) => [a++, b],
+//     back: 'back',
+//     left: 'left',
+//     right: 'right',
+// }
+// const { diagonalLeft, diagonalRight, backDiagonalLeft, backDiagonalRight, up, back, left, right } = moves
 
 const pieces = {
-    pawn: (a, b) => board[a + 1][b] == 'empty' ? 'can move' : 'cannot',
-    knight: () => 'knightFun',
-    bishop: () => 'bishopFun',
-    queen: () => 'Queen function',
-    king: () => 'kingFun',
-    rook: () => 'rookFun',
+    pawn: (a, b) => pawnMoves(a, b),
+    knight: 'knightFun',
+    bishop: 'bishopFun',
+    queen: 'Queen function',
+    king: 'kingFun',
+    rook: 'rookFun',
+    opPawn: 'opPawn',
+    opKnight: 'opKnight',
+    opBishop: 'opBishop',
+    opQueen: 'opQueen',
+    opKing: 'opKing',
+    opRook: 'opRook',
     x: 'empty'
 }
-const { pawn, knight, bishop, queen, king, rook, x } = pieces
+const { pawn, knight, bishop, queen, king, rook, opPawn, opKnight, opBishop, opQueen, opKing, opRook, x } = pieces
 
 const board = [
     [rook, knight, bishop, queen, king, bishop, knight, rook],
-    [pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn],
+    [pawn, pawn, pawn, pawn, pawn, opPawn, pawn, opPawn],
     [x, x, x, x, x, x, x, x],
     [x, x, x, x, x, x, x, x],
     [x, x, x, x, x, x, x, x],
     [x, x, x, x, x, x, x, x],
-    [pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn],
-    [rook, knight, bishop, queen, king, bishop, knight, rook],]
+    [opPawn, opPawn, opPawn, opPawn, opPawn, opPawn, opPawn, opPawn],
+    [opRook, opKnight, opBishop, opQueen, opKing, opBishop, opKnight, opRook],]
+
+const pawnMoves = (a, b) => {
+    let poss = []
+    if (a == 1 && !board[a + 1][b].includes('op')) poss = [...poss, [a + 1, b]]
+
+    return poss.flat(0)
+}
 
 const possible_moves = (a, b) => board[a][b](a, b)
+
 
 possible_moves(1, 6)//?
 
